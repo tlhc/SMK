4c698d83c9001a90c90ce17f38b7afa40c83ffee
tlhc <marst629@gmail.com>
2015-03-03 14:26:30 +0800

    update for vps

--- send_mail/send_mail.py
+++ send_mail/send_mail.py
@@ -9,6 +9,15 @@ from mail import MailInfo
 from AppExp import AppExp
 from configreader import MailCfgReader, ConfigObject
 
+def getip():
+    """ for my vps the config file use 0.0.0.0 as server address
+        so need to get the ipaddress
+        http://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib """
+    import socket
+    return ([(s.connect(('8.8.8.8', 80)), \
+            s.getsockname()[0], s.close()) \
+            for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1])
+
 def send_signal_section(item):
     """ send a config file content to list of mails """
     def send_file_content(server, fromaddr, toaddr, passwd, msg):
@@ -17,6 +26,8 @@ def send_signal_section(item):
         email.send()
     with open(item.confpath, 'r') as fhander:
         msg = fhander.read()
+        msg += '\n'
+        msg += getip()
         [send_file_content(item.smtpserver, \
                 item.fromaddress, toaddr, item.emailpwd, msg) \
                 for toaddr in item.toaddresslist]
